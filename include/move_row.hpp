a57aebef44e4ab677bdaabe00ae7ce44dcc8994e
mzakeri1 <mzakeri1@login01.rockfish.cluster>
2022-09-21 20:40:27 -0400

    Improve the performance by inlining and -O2 flag

--- include/move_row.hpp
+++ include/move_row.hpp
@@ -3,6 +3,10 @@
 
 #include <iostream>
 
+const uint32_t mask_p = ~((1U << 8) - 1);
+const uint32_t mask_pp = ~(((1U << 8) - 1) << 8);
+const uint32_t mask_id = ~(((1U << 8) - 1) << 16);
+
 class MoveRow{
     public:
         MoveRow () { p = 0; n = 0; pp = 0; id = 0; overflow_bits = 0;}
@@ -10,14 +14,14 @@ class MoveRow{
         void init(uint64_t p_, uint16_t n_, uint64_t pp_, uint64_t id_);
 
         void set_p(uint64_t p_);
-        void set_n(uint16_t n_) {n = n_;}
+        void set_n(uint16_t n_);
         void set_pp(uint64_t pp_);
         void set_id(uint64_t id_);
 
-        uint64_t get_p();
-        uint16_t get_n() {return n;}
-        uint64_t get_pp();
-        uint64_t get_id();
+        uint64_t get_p() const;
+        uint16_t get_n() const;
+        uint64_t get_pp() const;
+        uint64_t get_id() const;
     private:
         uint32_t p;
         uint16_t n;
@@ -26,4 +30,47 @@ class MoveRow{
         uint32_t overflow_bits;
 };
 
+inline uint64_t MoveRow::get_p() const{
+    if (overflow_bits != 0) {
+        uint32_t a = overflow_bits & (~mask_p);
+        uint64_t b = a;
+        b = (b << 32);
+        uint64_t c = p;
+        c = c | b;
+        return c;
+    } else {
+        return static_cast<uint32_t>(p);
+    }
+}
+
+inline uint16_t MoveRow::get_n() const{
+    return n;
+}
+
+inline uint64_t MoveRow::get_pp() const{
+    if (overflow_bits != 0) {
+        uint32_t a = (overflow_bits & (~mask_pp)) >> 8;
+        uint64_t b = a;
+        b = (b << 32);
+        uint64_t c = pp;
+        c = c | b;
+        return c;
+    } else {
+        return static_cast<uint32_t>(pp);
+    }
+}
+
+inline uint64_t MoveRow::get_id() const{
+    if (overflow_bits != 0) {
+        uint32_t a = (overflow_bits & (~mask_id)) >> 16;
+        uint64_t b = a;
+        b = (b << 32);
+        uint64_t c = id;
+        c = c | b;
+        return c;
+    } else {
+        return static_cast<uint32_t>(id);
+    }
+    return id;
+}
 #endif
--- src/move_row.cpp
+++ src/move_row.cpp
@@ -3,10 +3,6 @@
 
 #include "move_row.hpp"
 
-uint32_t mask_p = ~((1U << 8) - 1);
-uint32_t mask_pp = ~(((1U << 8) - 1) << 8);
-uint32_t mask_id = ~(((1U << 8) - 1) << 16);
-
 MoveRow::MoveRow(uint64_t p_, uint16_t n_, uint64_t pp_, uint64_t id_) {
     this->init(p_, n_, pp_, id_);
 }
@@ -25,6 +21,10 @@ void MoveRow::set_p(uint64_t p_) {
     overflow_bits = overflow_bits | (p_ >> 32);
 }
 
+void MoveRow::set_n(uint16_t n_) {
+    n = n_;
+}
+
 void MoveRow::set_pp(uint64_t pp_) {
     pp = pp_;
     overflow_bits = overflow_bits & mask_pp;
@@ -35,31 +35,4 @@ void MoveRow::set_id(uint64_t id_) {
     id = id_;
     overflow_bits = overflow_bits & mask_id;
     overflow_bits = overflow_bits | ((id_ >> 32) << 16);
-}
-
-uint64_t MoveRow::get_p() {
-    uint32_t a = overflow_bits & (~mask_p);
-    uint64_t b = a;
-    b = (b << 32);
-    uint64_t c = p;
-    c = c | b;
-    return c;
-}
-
-uint64_t MoveRow::get_pp() {
-    uint32_t a = (overflow_bits & (~mask_pp)) >> 8;
-    uint64_t b = a;
-    b = (b << 32);
-    uint64_t c = pp;
-    c = c | b;
-    return c;
-}
-
-uint64_t MoveRow::get_id() {
-    uint32_t a = (overflow_bits & (~mask_id)) >> 16;
-    uint64_t b = a;
-    b = (b << 32);
-    uint64_t c = id;
-    c = c | b;
-    return c;
 }
