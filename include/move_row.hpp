f4704bbb29d01e0ffbf48ca226c6ae20bb17889b
mohsenzakeri <mohs.zakeri@gmail.com>
2024-05-08 20:41:54 -0400

    Reduce the size of movi rows down to 12 bytes

--- include/move_row.hpp
+++ include/move_row.hpp
@@ -9,20 +9,15 @@
 // #define MODE -1// 0: regular, 1: constant, 2: one-bit
 // #endif
 
-// const uint32_t mask_p = ~((1U << 8) - 1);
-// const uint32_t mask_pp = ~(((1U << 8) - 1) << 8);
-// const uint32_t mask_id = ~(((1U << 8) - 1) << 16);
-// const uint32_t mask_c = ~(((1U << 8) - 1) << 24);
+const uint8_t mask_thresholds1 = static_cast<uint8_t>(~(((1U << 2) - 1) << 0)); // 00000011
+const uint8_t mask_thresholds2 = static_cast<uint8_t>(~(((1U << 2) - 1) << 2)); // 00001100
+const uint8_t mask_thresholds3 = static_cast<uint8_t>(~(((1U << 2) - 1) << 4)); // 00110000
+const uint8_t mask_c = static_cast<uint8_t>(~(((1U << 2) - 1) << 6));           // 11000000
 
-const uint16_t mask_thresholds1 = static_cast<uint16_t>(~(((1U << 2) - 1) << 0));
-const uint16_t mask_thresholds2 = static_cast<uint16_t>(~(((1U << 2) - 1) << 2));
-const uint16_t mask_thresholds3 = static_cast<uint16_t>(~(((1U << 2) - 1) << 4));
-
-const uint16_t mask_id =  static_cast<uint16_t>(~((1U << 8) - 1));
-const uint16_t mask_c = static_cast<uint16_t>(~(((1U << 2) - 1) << 8));
-const uint16_t mask_overflow_n = static_cast<uint16_t>(~(((1U << 1) - 1) << 10));
-const uint16_t mask_overflow_offset = static_cast<uint16_t>(~(((1U << 1) - 1) << 11));
-const uint16_t mask_overflow_thresholds = static_cast<uint16_t>(~(((1U << 1) - 1) << 12));
+const uint8_t mask_id =  static_cast<uint8_t>(~(((1U << 4) - 1) << 0));                  // 00001111
+const uint8_t mask_overflow_n = static_cast<uint8_t>(~(((1U << 1) - 1) << 4));           // 00010000
+const uint8_t mask_overflow_offset = static_cast<uint8_t>(~(((1U << 1) - 1) << 5));      // 00100000
+const uint8_t mask_overflow_thresholds = static_cast<uint8_t>(~(((1U << 1) - 1) << 6));  // 01000000
 
 class MoveRow{
     public:
@@ -78,23 +73,22 @@ class MoveRow{
 
         uint64_t row_size() {
 #if MODE == 0
-            return 16;
+            return 12;
 #endif
 #if MODE == 1
-            return 28;
+            return 24;
 #endif
 #if MODE == 2
             return 12;
 #endif
         }
     private:
-        // offset based: uint32_t p; // bwt row of the head before the jump
-        // offset based: uint32_t pp; // bwt row of the head after the jump
-        uint16_t offset; // offset of the bwt row head of the current run in the new run after the jump
-        uint16_t overflow_bits;
-        uint16_t n; // length of the run
         uint32_t id; // bwt run after the jump
+        uint16_t n; // length of the run
+        uint16_t offset; // offset of the bwt row head of the current run in the new run after the jump
+        uint16_t threshold;
 
+        uint8_t overflow_bits;
         // thresholds for all the rows:
 #if MODE == 0 or MODE == 1
         // uint16_t thresholds[3];
@@ -106,10 +100,6 @@ class MoveRow{
         uint16_t next_up[3];
         uint16_t next_down[3];
 #endif
-
-// #if MODE == 2
-        uint16_t threshold;
-// #endif
 };
 
 /* inline uint64_t MoveRow::get_p() const{
@@ -139,7 +129,7 @@ class MoveRow{
 }*/
 
 inline uint8_t MoveRow::get_threshold_status(uint16_t i) const {
-    const uint16_t mask_thresholds = static_cast<uint16_t>(~(((1U << 2) - 1) << i*2));
+    const uint8_t mask_thresholds = static_cast<uint8_t>(~(((1U << 2) - 1) << i*2));
     uint8_t status = static_cast<uint8_t>((thresholds_status & (~mask_thresholds)) >> i*2);
     return status;
 }
@@ -154,6 +144,11 @@ inline uint16_t MoveRow::get_n_ff() const{
 
 inline uint16_t MoveRow::get_offset() const{
     return offset;
+}
+
+inline uint8_t extract_value(uint8_t source, uint8_t mask, uint16_t offset_bits) {
+    uint8_t res = (source & (~mask)) >> offset_bits;
+    return res;
 }
 
 inline uint64_t MoveRow::get_id() const{
@@ -171,45 +166,22 @@ inline uint64_t MoveRow::get_id() const{
 }
 
 inline char MoveRow::get_c() const{
-    uint8_t a = static_cast<uint8_t>((overflow_bits & (~mask_c)) >> 8);
-    char c = static_cast<char>(a);
-    return c;
-}
-
-inline char MoveRow::get_c_jj() const{
-    uint8_t a = static_cast<uint8_t>((overflow_bits & (~mask_c)) >> 8);
-    char c = static_cast<char>(a);
-    return c;
-}
-
-inline char MoveRow::get_c_mm() const{
-    uint8_t a = static_cast<uint8_t>((overflow_bits & (~mask_c)) >> 8);
-    char c = static_cast<char>(a);
-    return c;
+    return static_cast<char>(extract_value(thresholds_status, mask_c, 6));
 }
 
 inline bool MoveRow::is_overflow_n() const{
-    uint32_t a = (overflow_bits & (~mask_overflow_n)) >> 10;
-    bool b = static_cast<bool>(a);
-    return !b;
-}
-
-inline bool MoveRow::is_overflow_n_ff() const{
-    uint32_t a = (overflow_bits & (~mask_overflow_n)) >> 10;
-    bool b = static_cast<bool>(a);
-    return !b;
+    uint8_t res = extract_value(overflow_bits, mask_overflow_n, 4);
+    return !static_cast<bool>(res);
 }
 
 inline bool MoveRow::is_overflow_offset() const{
-    uint32_t a = (overflow_bits & (~mask_overflow_offset)) >> 11;
-    bool b = static_cast<bool>(a);
-    return !b;
+    uint8_t res = extract_value(overflow_bits, mask_overflow_offset, 5);
+    return !static_cast<bool>(res);
 }
 
 inline bool MoveRow::is_overflow_thresholds() const{
-    uint32_t a = (overflow_bits & (~mask_overflow_thresholds)) >> 12;
-    bool b = static_cast<bool>(a);
-    return !b;
+    uint8_t res = extract_value(overflow_bits, mask_overflow_thresholds, 6);
+    return !static_cast<bool>(res);
 }
 
 #endif
