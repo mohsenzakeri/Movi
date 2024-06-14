0567019529de3cdae00019da23939d603b307153
mohsenzakeri <mohs.zakeri@gmail.com>
2024-06-14 08:31:32 -0400

    add test option with the small reference

--- test_data/test_pmls.sh
+++ test_data/test_pmls.sh
@@ -1,10 +1,17 @@
+if [ ! -z "$3" ]; then
+    expected=$3
+    echo "A"
+else
+    expected="../test_data/expected_pmls"
+    echo "B"
+fi
 index_dir=$1
 reads="../test_data/reads.fasta"
 movi_dir="../build"
 $movi_dir/movi-default query --pml --index $index_dir --read $reads
 $movi_dir/movi-default view --pml-file $reads.default.mpml.bin > test_out
 ls -l ../test_data/*.bin
-diff ../test_data/expected_pmls test_out | wc -l
+diff $expected test_out | wc -l
 
 
 index_dir=$2
