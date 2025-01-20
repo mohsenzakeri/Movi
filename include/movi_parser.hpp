3d89f4a5954cf7945fecc975794985003b26113a
mohsenzakeri <mohs.zakeri@gmail.com>
2025-01-20 15:00:13 -0500

    Reorganize movi parse code

---
+++ include/movi_parser.hpp
@@ -0,0 +1,13 @@
+#ifndef MOVI_PARSER_HPP
+#define MOVI_PARSER_HPP
+
+#include <iostream>
+
+#include <cxxopts.hpp>
+
+#include "utils.hpp"
+#include "movi_options.hpp"
+
+bool parse_command(int argc, char** argv, MoviOptions& movi_options);
+
+#endif
